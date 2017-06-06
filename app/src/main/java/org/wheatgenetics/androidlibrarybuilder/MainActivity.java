5bfc37461061a84a3cf80277c1108ba90840f0ca
Peter Rosario <PeterRosario@ksu.edu>
2017-06-06 10:04:14 -0500

    Added code to test BarcodeScanner.

--- app/src/main/java/org/wheatgenetics/androidlibrarybuilder/MainActivity.java
+++ app/src/main/java/org/wheatgenetics/androidlibrarybuilder/MainActivity.java
@@ -6,26 +6,31 @@ package org.wheatgenetics.androidlibrarybuilder;
  * android.support.v7.app.AppCompatActivity
  * android.view.Menu
  * android.view.MenuInflater
+ * android.view.MenuItem
  * android.widget.TextView
  *
  * org.wheatgenetics.androidlibrary.R
  * org.wheatgenetics.androidlibrarybuilder.R
  * org.wheatgenetics.javalib.Utils
+ * org.wheatgenetics.zxing.BarcodeScanner
  */
 
 public class MainActivity extends android.support.v7.app.AppCompatActivity
 {
+    private android.widget.TextView                textView;
+    private org.wheatgenetics.zxing.BarcodeScanner barcodeScanner = null;
+
     @java.lang.Override
     protected void onCreate(final android.os.Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         this.setContentView(org.wheatgenetics.androidlibrarybuilder.R.layout.activity_main);
 
-        final int                     number   = 2;
-        final android.widget.TextView textView = (android.widget.TextView)
+        this.textView = (android.widget.TextView)
             this.findViewById(org.wheatgenetics.androidlibrarybuilder.R.id.textView);
-        assert null != textView;
-        textView.setText(java.lang.String.format("doubleOf(doubleOf(%d)) is %d", number,
+        assert null != this.textView;
+        final int number = 2;
+        this.textView.setText(java.lang.String.format("doubleOf(doubleOf(%d)) is %d", number,
             org.wheatgenetics.androidlibrary.Utils.doubleOf(
                 org.wheatgenetics.javalib.Utils.doubleOf(number))));
     }
@@ -40,4 +45,30 @@ public class MainActivity extends android.support.v7.app.AppCompatActivity
         return true;
     }
 
+    @java.lang.Override
+    public boolean onOptionsItemSelected(final android.view.MenuItem item)
+    {
+        assert null != item;
+        switch (item.getItemId())
+        {
+            case org.wheatgenetics.androidlibrary.R.id.cameraOptionsMenuItem:
+                if (null == this.barcodeScanner)
+                    this.barcodeScanner = new org.wheatgenetics.zxing.BarcodeScanner(this);
+                this.barcodeScanner.scan();
+                break;
+        }
+        return true;
+    }
+
+    @java.lang.Override
+    protected void onActivityResult(final int requestCode,
+    final int resultCode, final android.content.Intent data)
+    {
+        java.lang.String barcode = org.wheatgenetics.zxing.BarcodeScanner.parseActivityResult(
+            requestCode, resultCode, data);
+        if (null == barcode) barcode = "null";
+
+        assert null != this.textView;
+        this.textView.setText(barcode);
+    }
 }
