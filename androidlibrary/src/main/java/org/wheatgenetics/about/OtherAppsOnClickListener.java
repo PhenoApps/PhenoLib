ae96347cc56f29ad6a61e02e6c18fe5e597b87f4
Peter Rosario <PeterRosario@ksu.edu>
2017-07-20 11:12:26 -0500

    OtherAppsOnClickListener: Added.

---
+++ androidlibrary/src/main/java/org/wheatgenetics/about/OtherAppsOnClickListener.java
@@ -0,0 +1,44 @@
+package org.wheatgenetics.about;
+
+/**
+ * Uses:
+ * android.content.Context
+ * android.view.View
+ * android.view.View.OnClickListener
+ *
+ * org.wheatgenetics.about.OtherApps.Index
+ * org.wheatgenetics.about.OtherAppsAlertDialog
+ */
+
+public class OtherAppsOnClickListener extends java.lang.Object
+implements android.view.View.OnClickListener
+{
+    // region Fields
+    private final android.content.Context                 context      ;
+    private final org.wheatgenetics.about.OtherApps.Index suppressIndex;
+
+    private org.wheatgenetics.about.OtherAppsAlertDialog otherAppsAlertDialog = null;
+    // endregion
+
+    private void showOtherAppsAlertDialog()
+    {
+        if (null == this.otherAppsAlertDialog) this.otherAppsAlertDialog =
+            new org.wheatgenetics.about.OtherAppsAlertDialog(this.context, this.suppressIndex);
+        this.otherAppsAlertDialog.show();
+    }
+
+    public OtherAppsOnClickListener(final android.content.Context context,
+    final org.wheatgenetics.about.OtherApps.Index suppressIndex)
+    {
+        super();
+
+        assert null != context;
+        this.context = context;
+
+        this.suppressIndex = suppressIndex;
+    }
+
+    @java.lang.Override
+    public void onClick(final android.view.View v)
+    { org.wheatgenetics.about.OtherAppsOnClickListener.this.showOtherAppsAlertDialog(); }
+}
