c856c2d0544a65e3587d7da2c284f751f6f8527e
Peter Rosario <PeterRosario@ksu.edu>
2017-09-26 17:09:58 -0500

    Refactored.

--- app/src/main/java/org/wheatgenetics/androidlibrarybuilder/MainActivity.java
+++ app/src/main/java/org/wheatgenetics/androidlibrarybuilder/MainActivity.java
@@ -1,15 +1,18 @@
 package org.wheatgenetics.androidlibrarybuilder;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity
+/**
+ * Uses:
+ * android.os.Bundle
+ * android.support.v7.app.AppCompatActivity
+ *
+ * org.wheatgenetics.androidlibrarybuilder.R
+ */
+public class MainActivity extends android.support.v7.app.AppCompatActivity
 {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
+    @java.lang.Override
+    protected void onCreate(final android.os.Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        this.setContentView(org.wheatgenetics.androidlibrarybuilder.R.layout.activity_main);
     }
 }
