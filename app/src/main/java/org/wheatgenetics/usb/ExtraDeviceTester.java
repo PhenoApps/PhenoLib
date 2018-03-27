3e5bb8205bf1316046e62618b9ab484e2122da9d
Peter Rosario <PeterRosario@ksu.edu>
2018-03-27 10:16:35 -0500

    Improved code consistency. Used more annotations.

--- app/src/main/java/org/wheatgenetics/usb/ExtraDeviceTester.java
+++ app/src/main/java/org/wheatgenetics/usb/ExtraDeviceTester.java
@@ -4,12 +4,14 @@ package org.wheatgenetics.usb;
  * Uses:
  * android.app.Activity
  * android.support.annotation.NonNull
+ * android.support.annotation.RestrictTo
+ * android.support.annotation.RestrictTo.Scope
  *
  * org.wheatgenetics.usb.Device.Exception
  * org.wheatgenetics.usb.ExtraDevice
  * org.wheatgenetics.usb.Scale
  */
-@java.lang.SuppressWarnings("ClassExplicitlyExtendsObject")
+@java.lang.SuppressWarnings({"ClassExplicitlyExtendsObject"})
 public class ExtraDeviceTester extends java.lang.Object
 {
     // region Fields
@@ -18,29 +20,32 @@ public class ExtraDeviceTester extends java.lang.Object
     // endregion
 
     // region Package Methods
+    @android.support.annotation.RestrictTo(android.support.annotation.RestrictTo.Scope.SUBCLASSES)
     android.app.Activity getActivity() { return this.activity; }
 
+    @android.support.annotation.RestrictTo(android.support.annotation.RestrictTo.Scope.SUBCLASSES)
     org.wheatgenetics.usb.ExtraDevice extraDevice()
     {
         if (null == this.extraDeviceInstance)
-            this.extraDeviceInstance = new org.wheatgenetics.usb.ExtraDevice(this.getActivity(),
+            this.extraDeviceInstance = new org.wheatgenetics.usb.ExtraDevice(
+                this.getActivity()                         ,
                 org.wheatgenetics.usb.Scale.ELANEVendorId  ,
                 org.wheatgenetics.usb.Scale.ELANEProductIds);
         return this.extraDeviceInstance;
     }
     // endregion
 
-    // region Public Methods
     public ExtraDeviceTester(
     @android.support.annotation.NonNull final android.app.Activity activity)
     { super(); this.activity = activity; }
 
+    // region Public Methods
     public java.lang.String information()
     { return "information: " + this.extraDevice().information(); }
 
     public java.lang.String handlingFormattedRead()
     {
-        try { return this.extraDevice().formattedRead(); }
+        try { return this.extraDevice().formattedRead(); /* throws */ }
         catch (final org.wheatgenetics.usb.Device.Exception e) { return e.getMessage(); }
     }
     // endregion
