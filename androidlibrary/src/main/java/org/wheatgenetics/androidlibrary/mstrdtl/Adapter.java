b8231f0458b80fcab5d141fd15ff124bf746582d
Peter Rosario <PeterRosario@ksu.edu>
2018-12-14 15:19:04 -0600

    Adapter: Added.

---
+++ androidlibrary/src/main/java/org/wheatgenetics/androidlibrary/mstrdtl/Adapter.java
@@ -0,0 +1,55 @@
+package org.wheatgenetics.androidlibrary.mstrdtl;
+
+/**
+ * Uses:
+ * android.support.annotation.IntRange
+ * android.support.annotation.NonNull
+ * android.support.v7.widget.RecyclerView.Adapter
+ * android.view.LayoutInflater
+ * android.view.View
+ * android.view.View.OnClickListener
+ * android.view.ViewGroup
+ *
+ * org.wheatgenetics.javalib.mstrdtl.Items
+ *
+ * org.wheatgenetics.androidlibrary.R
+ *
+ * org.wheatgenetics.androidlibrary.mstrdtl.ViewHolder
+ */
+abstract class Adapter extends android.support.v7.widget.RecyclerView.Adapter<
+org.wheatgenetics.androidlibrary.mstrdtl.ViewHolder>
+{
+    private final org.wheatgenetics.javalib.mstrdtl.Items items;
+
+    abstract android.view.View.OnClickListener makeOnClickListener(
+    @android.support.annotation.IntRange(
+        from = org.wheatgenetics.javalib.mstrdtl.Item.MIN_POSITION) final int position);
+
+    Adapter(@android.support.annotation.NonNull final org.wheatgenetics.javalib.mstrdtl.Items items)
+    { super(); this.items = items; }
+
+    // region Overridden Methods
+    @java.lang.Override @android.support.annotation.NonNull
+    public org.wheatgenetics.androidlibrary.mstrdtl.ViewHolder onCreateViewHolder(
+    @android.support.annotation.NonNull final android.view.ViewGroup parent, final int viewType)
+    {
+        return new org.wheatgenetics.androidlibrary.mstrdtl.ViewHolder(
+            android.view.LayoutInflater.from(parent.getContext()).inflate(
+                org.wheatgenetics.androidlibrary.R.layout.mstrdtl_list_item,
+                parent, /* attachToRoot => */false));
+    }
+
+    @java.lang.Override public void onBindViewHolder(@android.support.annotation.NonNull
+    final org.wheatgenetics.androidlibrary.mstrdtl.ViewHolder viewHolder,
+    final int                             position                      )
+    {
+        assert null != this.items;
+        viewHolder.bind(
+            /* item            => */ this.items.get(position)                             ,
+            /* onClickListener => */ this.makeOnClickListener(position) /* polymorphism */);
+    }
+
+    @java.lang.Override public int getItemCount()
+    { return null == this.items ? 0 : this.items.size(); }
+    // endregion
+}
