549d2ecc42367b7f86045bb9ae6e3e06a801f528
Peter Rosario <PeterRosario@ksu.edu>
2018-12-27 12:00:38 -0600

    Items: After creating ITEMS_INSTANCE populated it with 25 Items. MainActivity: Started showing ListActivity.

--- app/src/main/java/org/wheatgenetics/androidlibrarybuilder/MainActivity.java
+++ app/src/main/java/org/wheatgenetics/androidlibrarybuilder/MainActivity.java
@@ -44,6 +44,8 @@ package org.wheatgenetics.androidlibrarybuilder;
  * org.wheatgenetics.usb.ScaleTester
  * org.wheatgenetics.zxing.BarcodeScanner
  *
+ * org.wheatgenetics.androidlibrarybuilder.mstrdtl.ListActivity
+ *
  * org.wheatgenetics.androidlibrarybuilder.BuildConfig
  * org.wheatgenetics.androidlibrarybuilder.R
  * org.wheatgenetics.androidlibrarybuilder.WebViewActivity
@@ -75,7 +77,7 @@ implements org.wheatgenetics.androidlibrary.DebouncingEditorActionListener.Recei
         otherAppsButtonClickCount = 0, deviceListButtonClickCount  = 0,
         scaleButtonClickCount     = 0, scaleReaderButtonClickCount = 0;
 
-    private android.content.Intent webIntentInstance = null;
+    private android.content.Intent webIntentInstance = null, listIntentInstance = null;
     // endregion
 
     // region Private Methods
@@ -168,6 +170,7 @@ implements org.wheatgenetics.androidlibrary.DebouncingEditorActionListener.Recei
         }
     }
 
+    // region Intent Private Methods
     private android.content.Intent webIntent(
     final java.lang.String content, final java.lang.String encoding)
     {
@@ -181,6 +184,15 @@ implements org.wheatgenetics.androidlibrary.DebouncingEditorActionListener.Recei
 
         return this.webIntentInstance;
     }
+
+    private android.content.Intent listIntent()
+    {
+        if (null == this.listIntentInstance) this.listIntentInstance =
+            new android.content.Intent(this,
+                org.wheatgenetics.androidlibrarybuilder.mstrdtl.ListActivity.class);
+        return this.listIntentInstance;
+    }
+    // endregion
 
     private void showChangeLog()
     {
@@ -304,7 +316,7 @@ implements org.wheatgenetics.androidlibrary.DebouncingEditorActionListener.Recei
                 break;
 
             case 1:
-                org.wheatgenetics.androidlibrary.Utils.showLongToast (this,"long" );
+                org.wheatgenetics.androidlibrary.Utils.showLongToast(this,"long" );
                 break;
 
             case 2:                 this.listAll(this.permissionDir); break;
@@ -331,12 +343,14 @@ implements org.wheatgenetics.androidlibrary.DebouncingEditorActionListener.Recei
                         response.content(), response.contentEncoding()));
                 break;
 
-            case 7: this.showChangeLog(); break;
+            case 7: this.startActivity(this.listIntent    ()); break;
+            case 8: this.showChangeLog()                     ; break;
         }
 
         switch (this.buttonClickCount)
         {
-            case 0: case 1: case 2: case 3: case 4: case 5: case 6: this.buttonClickCount++; break;
+            case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
+                this.buttonClickCount++; break;
             default: this.buttonClickCount = 0; break;
         }
 
@@ -349,7 +363,8 @@ implements org.wheatgenetics.androidlibrary.DebouncingEditorActionListener.Recei
             case 4: this.setButtonText("requestDir.list() 2 of 3"); break;
             case 5: this.setButtonText("requestDir.list() 3 of 3"); break;
             case 6: this.setButtonText("http://www.example.org/" ); break;
-            case 7: this.setButtonText("ChangeLog"               ); break;
+            case 7: this.setButtonText("Master-Detail Flow"      ); break;
+            case 8: this.setButtonText("ChangeLog"               ); break;
         }
     }
 
